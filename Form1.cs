e869628f15d983cffcc9881ffda20dc549beee5f
Amna Imran <amnaimran481@gmail.com>
2025-06-01 02:28:32 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BANK_MANAGEMENT_SYSTEM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-FT6QK8Q\\SQLEXPRESS;Initial Catalog=\"Bank DB\";Integrated Security=True;Trust Server Certificate=True");
+            conn.Open();
+
+
+
+          
+
+             string username = textusername.Text.Trim();
+            string password = textpassword.Text;
+
+            
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT COUNT(1) FROM User WHERE Username=@username AND Password=@password";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    if (count == 1)
+                    {
+                        MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        // Open the main dashboard form
+                        // MainForm main = new MainForm();
+                        // main.Show();
+                        // this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid username or password.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+    }
+}
+
+
+
+
+
+        
+
+        
+                
+    
+
---
+++ Form4.cs
@@ -0,0 +1,75 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace BankingSystem
+{
+    public partial class EmployeeForm : Form
+    {
+        string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=BankingDB;Integrated Security=True";
+
+        public EmployeeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void EmployeeForm_Load(object sender, EventArgs e)
+        {
+            LoadEmployees();
+        }
+
+        private void LoadEmployees()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Employees", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvEmployees.DataSource = dt;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Employees (Name, Designation, Contact, Salary) VALUES (@Name, @Designation, @Contact, @Salary)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Name", txtNAme.Text);
+                cmd.Parameters.AddWithValue("@Designation", txtDesignation.Text);
+                cmd.Parameters.AddWithValue("@Contact", txtContact.Text);
+                cmd.Parameters.AddWithValue("@Salary", Convert.ToDecimal(txtSalary.Text));
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Employee added successfully.");
+                LoadEmployees();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dgvEmployees.CurrentRow != null)
+            {
+                int empId = Convert.ToInt32(dgvEmployees.CurrentRow.Cells["EmployeeID"].Value);
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "UPDATE Employees SET Name=@Name, Designation=@Designation, Contact=@Contact, Salary=@Salary WHERE EmployeeID=@EmployeeID";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@EmployeeID", empId);
+                    cmd.Parameters.AddWithValue("@Name", txtEmpName.Text);
+                    cmd.Parameters.AddWithValue("@Designation", txtDesignation.Text);
+                    cmd.Parameters.AddWithValue("@Contact", txtContact.Text);
+                    cmd.Parameters.AddWithValue("@Salary", Convert.ToDecimal(txtSalary.Text));
